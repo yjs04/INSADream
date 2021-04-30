9367440a9656721012186245c0b9b1489f711654
yjs04 <yjsss04@kakao.com>
2021-04-30 13:50:03 +0900

    2021-04-30

--- resource/js/app.js
+++ resource/js/app.js
@@ -22,7 +22,20 @@ class App{
 
         $("#all_menu_close").click(()=>{
             $(".all_menu_area").removeClass("on");
-        })
+        });
+
+        $("#slider-left").click(()=>{
+            $(".visual_box").css({"transform": "translateX(0)"});
+            $("#slider-left").css({"pointer-events":"none"});
+            $("#slider-right").css({"pointer-events":"all"});
+        });
+
+        $("#slider-right").click(()=>{
+            $(".visual_box").css({"transform": "translateX(-50%)"});
+            $("#slider-right").css({"pointer-events":"none"});
+            $("#slider-left").css({"pointer-events":"all"});
+        });
+        
     }
 }
 
