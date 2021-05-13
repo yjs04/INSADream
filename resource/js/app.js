55ae12135ff6317785c37dc8ce4a6f54f44f8c0f
yjs04 <yjsss04@kakao.com>
2021-05-13 15:41:43 +0900

    2021-05-13

--- resource/js/app.js
+++ resource/js/app.js
@@ -18,10 +18,12 @@ class App{
 
         $("#all_menu_open").click(()=>{
             $(".all_menu_area").addClass("on");
+            $("body").css({"overflow":"hidden"});
         });
 
         $("#all_menu_close").click(()=>{
             $(".all_menu_area").removeClass("on");
+            $("body").css({"overflow":"auto"});
         });
 
         $("#slider-left").click(()=>{
@@ -34,6 +36,17 @@ class App{
             $(".visual_box").css({"transform": "translateX(-50%)"});
             $("#slider-right").css({"pointer-events":"none"});
             $("#slider-left").css({"pointer-events":"all"});
+        });
+
+        let lastScrollTop = 0;
+        let headerHeight = $("header").outerHeight();
+        $(window).scroll(function(){
+            let st = $(this).scrollTop();
+            if(lastScrollTop < st && st > headerHeight) $("header").css({"top":`-${headerHeight}px`,"box-shadow":"none"});
+            else if(st == 0) $("header").css({"box-shadow":"none"});
+            else $("header").css({"top":"0px","box-shadow":"0 10px 20px rgba(0,0,0,0.1)"});
+
+            lastScrollTop = st;
         });
         
     }
